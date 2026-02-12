ec1fe5fe70482fb06991b021820513c77a7c75b1
Ady Gould (306) <Adrian.Gould@NMTafe.WA.edu.AU>
2026-02-12 13:03:16 +0800

    chore: start of BED 2026 S1 Demo application

---
+++ app/Http/Requests/UpdateContactRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // TODO: Modify to check authenticated
+        // TODO: Modify to check authorised / own the contact
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'given_name' => [
+                'sometimes',
+                'required',
+                'string',
+                'max:64'
+            ],
+            'family_name' => [
+                'sometimes',
+                'nullable',
+                'string',
+                'max:64'
+            ],
+            'nick_name' => [
+                'sometimes',
+                'nullable',
+                'string',
+                'max:32'
+            ],
+            'title' => [
+                'sometimes',
+                'nullable',
+                'string',
+                'max:16'
+            ],
+        ];
+    }
+}
