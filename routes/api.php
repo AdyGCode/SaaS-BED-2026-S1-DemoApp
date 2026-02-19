4ab7eb11557ebb334725e2dc305715a0302d0a20
Adrian Gould (Lec) <adrian.gould@nmtafe.wa.edu.au>
2026-02-19 11:44:13 +0800

    wip(Session-03): Pest Test

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/contacts', ContactController::class);
+// Automatically has /api in the prefix, we now add the /vx after...
+// route('api.v1.contacts.hello')
+Route::prefix('v1')
+    ->as('api.v1.')
+    ->group(function () {
+        Route::apiResource('contacts', ContactController::class);
+    });
---
+++ tests/Feature/ContactApiTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Contact;
+
+// uses(RefreshDatabase::class);
+
+it('returns a list of contacts', function () {
+    $response = $this->get('/api/v1/contacts');
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        '*' => ['id', 'given_name', 'family_name', 'nick_name', 'title'],
+    ]);
+});
+
+it('returns a single contact', function () {
+    $response = $this->get('/api/v1/contacts/1');
+    $response->assertStatus(200);
+    $response->assertJsonStructure(
+        ['id', 'given_name', 'family_name', 'nick_name', 'title']
+    );
+});
+
+it('returns a newly created contact', function () {
+    $attributes = Contact::factory()->raw();
+    $response = $this->post('/api/v1/contacts', $attributes);
+    $response->assertStatus(201)
+        ->assertJsonStructure(
+            ['id', 'given_name', 'family_name', 'nick_name', 'title']
+        );
+    $this->assertDatabaseHas('contacts', $attributes);
+});
+
+// TODO: Create the missing given name when creating a contact test
+it('returns create contact error when missing given name', function () {
+
+});
+
+// TODO: Create the update contact test
+it('returns a updated contact', function () {
+});
+
+// TODO: Create the delete contact test
+it('returns a deleted contact', function () {
+});
+
+
+
+
+
+// TODO: Create the missing given name when creating a contact test
+it('returns an error when new contact is missing given name', function () {
+
+    $attributes = Contact::factory()->raw();
+    unset($attributes['given_name']);
+    $response = $this->post('/api/v1/contacts', $attributes);
+    $response->assertStatus(302)
+        ->assertJsonStructure(
+            ['message',]
+        );
+    $this->assertDatabaseHas('contacts', $attributes);
+});
